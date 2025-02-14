6d8f75f603a1a264dd098fd0b9b42f66c6a4888a
Tom MacWright <tom@macwright.com>
2025-02-14 14:16:52 -0500

    Don't set selection in dispatch() call

--- src/inline-edit.ts
+++ src/inline-edit.ts
@@ -558,7 +558,6 @@ class InputWidget extends WidgetType {
             }),
             setLoading.of(false),
           ],
-          selection: EditorSelection.cursor(state.to),
         });
       } catch (error) {
         if (error instanceof DOMException && error.name === "AbortError") {
