6d91b83ca74d273353283eb09aea9a7a66c91a9c
Tom MacWright <tom@macwright.com>
2025-02-24 18:15:51 -0500

    Export more components so that consumers can mix & match
    - Fixes #26

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-export {
-  aiExtension,
-  showAiEditInput,
-  closeAiEditInput,
-  rejectAiEdit,
-  acceptAiEdit,
-} from "./inline-edit.js";
+export * from "./inline-edit.js";
+export * from "./state.js";
+export * from "./theme.js";
