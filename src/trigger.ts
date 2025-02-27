02d8f2bb9a3193f1b5860df1bd5e9efb9e15859b
Tom MacWright <tom@macwright.com>
2025-02-27 15:13:48 -0500

    Organization

--- src/trigger.ts
+++ src/trigger.ts
@@ -2,11 +2,54 @@ import { EditorView, type PluginValue, ViewPlugin, type ViewUpdate } from "@code
 import { showAiEditInput } from "./inline-edit.js";
 import { defaultKeymaps, inputState, optionsFacet } from "./state.js";
 import { formatKeymap } from "./utils.js";
+import { combineConfig, Facet } from "@codemirror/state";
+
+/**
+ * Options to customize trigger rendering.
+ */
+interface TriggerOptions {
+  render: (view: EditorView) => HTMLElement;
+}
+
+/**
+ * The default renderer, which renders a button that
+ * triggers AI selection mode.
+ */
+export function defaultTriggerRenderer(view: EditorView) {
+  const options = view.state.facet(optionsFacet);
+  const keymaps = { ...defaultKeymaps, ...options.keymaps };
+  const dom = document.createElement("div");
+  dom.className = "cm-ai-tooltip";
+  const tooltip = dom.appendChild(document.createElement("div"));
+  tooltip.className = "cm-ai-tooltip-button";
+  tooltip.innerHTML = `<span>Edit <span class="hotkey">${formatKeymap(keymaps.showInput)}</span></span>`;
+
+  // NOTE: preventing mousedown from propagating here prevents
+  // the tooltip from being closed before it can be clicked, but
+  // I'm still triggering the action on click to preserve native click-cancel
+  // behavior (dragging to cancel the click)
+  tooltip.querySelector("span")?.addEventListener("mousedown", (evt) => {
+    evt.stopPropagation();
+  });
+  tooltip.querySelector("span")?.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    showAiEditInput(view);
+  });
+  return tooltip;
+}
+
+export const triggerOptions = Facet.define<TriggerOptions, TriggerOptions>({
+  combine(value) {
+    return combineConfig(value, {
+      render: defaultTriggerRenderer,
+    });
+  },
+});
 
 export const triggerViewPlugin = ViewPlugin.fromClass(
   class TriggerPlugin implements PluginValue {
     suppress: boolean;
-    dom: HTMLDivElement;
+    dom: HTMLElement;
 
     /**
      * When the mouse is down, don't show tooltips
@@ -23,31 +66,17 @@ export const triggerViewPlugin = ViewPlugin.fromClass(
       this.display(this.view);
     };
 
+    /**
+     * Attach listeners and construct the initial view
+     * for the trigger UI.
+     */
     constructor(public view: EditorView) {
+      const options = view.state.facet(triggerOptions);
       this.suppress = false;
       document.addEventListener("mousedown", this.mousedown);
       document.addEventListener("mouseup", this.mouseup);
 
-      const options = view.state.facet(optionsFacet);
-      const keymaps = { ...defaultKeymaps, ...options.keymaps };
-      const dom = document.createElement("div");
-      dom.className = "cm-ai-tooltip";
-      const tooltip = dom.appendChild(document.createElement("div"));
-      tooltip.className = "cm-ai-tooltip-button";
-      tooltip.innerHTML = `<span>Edit <span class="hotkey">${formatKeymap(keymaps.showInput)}</span></span>`;
-
-      // NOTE: preventing mousedown from propagating here prevents
-      // the tooltip from being closed before it can be clicked, but
-      // I'm still triggering the action on click to preserve native click-cancel
-      // behavior (dragging to cancel the click)
-      tooltip.querySelector("span")?.addEventListener("mousedown", (evt) => {
-        evt.stopPropagation();
-      });
-      tooltip.querySelector("span")?.addEventListener("click", (evt) => {
-        evt.preventDefault();
-        showAiEditInput(view);
-      });
-
+      const tooltip = options.render(view);
       view.dom.appendChild(tooltip);
       this.dom = tooltip;
     }
@@ -67,37 +96,50 @@ export const triggerViewPlugin = ViewPlugin.fromClass(
         return;
       }
       view.requestMeasure({
-        read: (view) => {
-          const range = view.state.selection.ranges.find((range) => !range.empty);
-          if (range && !this.suppress) {
-            // Coords here are relative to the scrollable document.
-            const coords = view.coordsAtPos(range.from);
-            if (!coords) return;
-
-            this.dom.style.display = "flex";
-            const tooltipRect = this.dom.getBoundingClientRect();
-            const scrollRect = view.dom.getBoundingClientRect();
-            const domRect = view.dom.parentElement?.getBoundingClientRect();
-
-            // If the tooltip is slammed to the right side of the page,
-            // pull it back so that it isn't quite as slammed.
-            const left = Math.min(coords.left, scrollRect.width - tooltipRect.width);
-
-            // If the tooltip is in the overscrolled area at the top,
-            // try to show it just at the top. This relies on the parent
-            // of the codemirror container, which is not an ideal
-            // strategy.
-            let top = coords.top - tooltipRect.height;
-            top = domRect ? Math.max(domRect.y, top) : top;
-
-            this.dom.style.left = `${left}px`;
-            this.dom.style.top = `${top}px`;
-          } else {
-            this.dom.style.display = "none";
-          }
-        },
+        read: this.#onRead,
       });
     }
+
+    /**
+     * Method to place or move the trigger UI after we've asked
+     * CodeMirror for permission to read the DOM.
+     */
+    #onRead = (view: EditorView) => {
+      const range = view.state.selection.ranges.find((range) => !range.empty);
+      if (range && !this.suppress) {
+        // Coords here are relative to the scrollable document.
+        const coords = view.coordsAtPos(range.from);
+        if (!coords) return;
+
+        this.dom.style.display = "flex";
+
+        // These measurements are definitely slow and we don't want to
+        // do them very often! We may want to cache these in the future.
+        const tooltipRect = this.dom.getBoundingClientRect();
+        const scrollRect = view.dom.getBoundingClientRect();
+        const domRect = view.dom.parentElement?.getBoundingClientRect();
+
+        // The furthest right we want to place the tooltip, to avoid
+        // it getting smushed
+        const rightEdge = scrollRect.width - tooltipRect.width;
+
+        // If the tooltip is slammed to the right side of the page,
+        // pull it back so that it isn't quite as slammed.
+        const left = Math.min(coords.left, rightEdge);
+
+        // If the tooltip is in the overscrolled area at the top,
+        // try to show it just at the top. This relies on the parent
+        // of the codemirror container, which is not an ideal
+        // strategy.
+        let top = coords.top - tooltipRect.height;
+        top = domRect ? Math.max(domRect.y, top) : top;
+
+        this.dom.style.left = `${left}px`;
+        this.dom.style.top = `${top}px`;
+      } else {
+        this.dom.style.display = "none";
+      }
+    };
 
     destroy() {
       document.removeEventListener("mousedown", this.mousedown);
@@ -107,6 +149,9 @@ export const triggerViewPlugin = ViewPlugin.fromClass(
   },
 );
 
+/**
+ * Default theme for the UI.
+ */
 const triggerBaseTheme = EditorView.baseTheme({
   ".cm-ai-tooltip-button": {
     userSelect: "none",
